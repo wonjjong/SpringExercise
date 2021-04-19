de077dee84608b7cbf546dc409c5dc17cfd1f115
wonjjong <wonjjong.dev@gmail.com>
2021-04-19 13:00:29 +0900

    빈 라이프사이클 강의 코딩

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,15 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+
+    }
+
+    static class SingletonBean {
+
+    }
+}
